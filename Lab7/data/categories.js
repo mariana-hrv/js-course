7ac282bbc56bcc206961eda5f17ea580889a7b05
mariana-hrv <maryanagrynivv@gmail.com>
2026-04-26 23:33:22 +0300

    Create categories.js

---
+++ Lab7/data/categories.js
@@ -0,0 +1,28 @@
+{
+  "categories": [
+    {
+      "id": 1,
+      "name": "Піца",
+      "shortname": "pizza",
+      "notes": "Lorem ipsum dolor sit amet consectetur adipiscing elit."
+    },
+    {
+      "id": 2,
+      "name": "Напої",
+      "shortname": "drinks",
+      "notes": "Lorem ipsum dolor sit amet consectetur adipiscing elit."
+    },
+    {
+      "id": 3,
+      "name": "Десерти",
+      "shortname": "desserts",
+      "notes": "Lorem ipsum dolor sit amet consectetur adipiscing elit."
+    },
+    {
+      "id": 4,
+      "name": "Салати",
+      "shortname": "salads",
+      "notes": "Lorem ipsum dolor sit amet consectetur adipiscing elit."
+    }
+  ]
+}
